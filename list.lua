a9755881076f1e57fe28a6a5ce88dcbd6c1a9390
exius2u <58986835+exius2u@users.noreply.github.com>
2023-01-24 15:42:06 -0600

    Add files via upload
    adding the current blacklist names to the lists. many more to come initial 7 added in this update

--- list.lua
+++ list.lua
@@ -1,45 +1,48 @@
 local _, t = ...
 
--- Faerlina Discord Blocklist Table
+-- Faerlina Discord Blocklist Table, Available Categories are: "dungeon", "raid", "trade", "gdkp", "harassment"
 
 t.case_table = {
 	[0] = {
-		name = "Arckanto",
-		class = "HUNTER",
-		guid = "Player-4465-02FE73DB",
-		faction = "Horde",
-		level = 4,
-		category = "raid",
-		description = "Took item with RL from highest roller in 25 man",
-		url = "https://discord.com/channels/610036506974748700/1036473795608850472",
+		name = "Gamezero",
+		category = "trade",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
 	},
-	[1] = {
-		name = "Eilthyra",
-		guid = "Player-4465-024BA943",
-		faction = "Horde",
-		level = 3,
-		category = "dungeon",
-		description = "Admitted needing on epic plate gear to DE for themselves.",
-		url = "https://discord.com/channels/610036506974748700/1043854693358645388",
+	[0] = {
+		name = "Fearclapu",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
 	},
-	[2] = {
-		name = "Manisk",
-		class = "DRUID",
-		guid = "Player-4465-0311C5DA",
-		faction = "Horde",
-		level = 1,
-		category = "dungeon",
-		description = "Player ninjaed [Red Sword of Courage] he can't use from tank",
-		url = "https://discord.com/channels/610036506974748700/1064990197240041614",
+		[0] = {
+		name = "Kinkyssham",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
 	},
-	[3] = {
-		name = "Manisk",
-		class = "DRUID",
-		guid = "Player-4465-0311C5DA",
-		faction = "Horde",
-		level = 1,
-		category = "dungeon",
-		description = "Player ninjaed [Red Sword of Courage] he can't use from tank",
-		url = "https://discord.com/channels/610036506974748700/1064990197240041614",
+		[0] = {
+		name = "Minipojkejr",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
+	},
+		[0] = {
+		name = "Kinkyspriest",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
+	},
+		[0] = {
+		name = "Kinkysccbot",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
+	},
+		[0] = {
+		name = "Kinkyswarr",
+		category = "gdkp",
+		description = "Was tried and found Guilty in the Faerlina Discord court system. Exact offense detailed in the provided link.",
+		url = "https://discord.com/channels/757975796249002118/956696638829764677",
 	},
 }
